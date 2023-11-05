661fba72e347d7805f186d5808bdf923a9ce1094
Gustavs <me@gustavs.ovh>
2023-11-05 19:03:48 +0200

    Cenu maiņas, ievārījuma cukura funkcija

--- projekti/ziemas_krajumi_gustavs.py
+++ projekti/ziemas_krajumi_gustavs.py
@@ -3,41 +3,50 @@ aboli = float(input("Cik kg ābolu? [>=0]: "))
 ir_ievarijuma = (input("Tiks izmantots ievārījuma vai parastais cukurs? [i/p]: ") == "i") # Ja atbild i, tad pārveido uz bool
 if input("Vai jums ir labi ar sirdi? [j/n]: ") != 'j':
     exit()
-perkamie_produkti = []
 
 # Receptes proporcijas: 1 kg āboli : 300 g cukurs : 160 ml ūdens : 1/3 citrons : 1.6 ml mandeļu ekstrakts : 3.3 g kanēlis
 
-# Cenu karte proporcijās uz 1 kg āboliem, eiro.
 cenas = {
-    "cukurs": 0.5, # 300 g
-    "citrons": 0.13, # 1/3 
-    "ūdens": 0.07, # 160 ml 
-    "mandeļu ekstrakts": 0.16, # 1.6 ml 
-    "kanēlis": 0.24 # 3.3 g
+    "cukurs": 0.00126, # 1 g
+    "ievārījuma cukurs": 0.0028, # 1g
+    "citrons": 0.67, # 1 gab
+    "ūdens": 0.00017, # 1 ml 
+    "mandeļu ekstrakts": 0.08, # 1 ml 
+    "kanēlis": 0.03 # 1 g
+}
+
+recepte = {
+    "cukurs": 300.0,
+    "ievārījuma cukurs": 250.0,
+    "citrons": 0.3,
+    "ūdens": 160.0,
+    "mandeļu ekstrakts": 1.6,
+    "kanēlis": 3.3
 }
 
 produkti = {}
 
 # Pieejamo produktu pārbaude
 print('\t- Pieejamie produkti -')
-for produkts in ['cukurs','citrons', 'ūdens','mandeļu ekstrakts', 'kanēlis']: # Priekš katras sastāvdaļas pērkamo sarakstā, paprasa vai šis atrodas mājās
-    produkti[produkts] = input(f'Cik daudz mājās ir {produkts}? [daudz./0]: ')  
 
+vajadzigie = ['cukurs','citrons', 'ūdens','mandeļu ekstrakts', 'kanēlis']
+if ir_ievarijuma:
+    vajadzigie[0] = 'ievārījuma cukurs'
 
-# un parāda, tos ko jāpērk
-if perkamie_produkti != []:
-    print('! Vēl jānopērk: ',end='')
-    for produkts in perkamie_produkti:
-        print(produkts,end=', ')
-    print('.\n')
-else:
-    print('! Visi produkti ir pieejami')
+for produkts in vajadzigie: # Priekš katras sastāvdaļas pērkamo sarakstā, paprasa vai tas atrodas mājās
+    produkti[produkts] = float(input(f'Cik daudz mājās ir {produkts}? [daudz./0]: '))  
 
-print(f'\t- Apkopojums -\nPriekš {ceil(aboli)}kg āboliem vajadzīgs:')
-# Paņem katru produktu kas ir pērkamo sarakstā (par tiem, kas jau ir mājās nav jāmaksā)
+print('\t- Pirkumu saraksts -')
+
 kopsumma = 0
-for produkts in perkamie_produkti:
-    # Tā cenu iegūst no cenu kartes un sareizina ar ābolu daudzumu, jo visas cenas ir norādītas proporcijā ar 1 kg āboliem.
-    kopsumma += cenas[produkts] * aboli
-    print(f'{produkts} - {cenas[produkts] * aboli}€') 
-print(f'\t[ Summa: {ceil(kopsumma)}€ ]')
+
+for produkts in produkti:
+    if (recepte[produkts] * aboli - produkti[produkts]) <= 0: # Atņem jau esošā produkta daudzumu no vajadzīgā, lai uzzinātu, vai vēl jāpērk
+        print(f'* {produkts} nav jāpērk, mājās ir pietiekami.')
+    else:
+        # Ar formulu (<vajadzīgais daudzums> - <esošais daudzums>) * cena aprēķina tikai vajadzīgā produkta cenu, to noapaļo
+        vajadziga_cena = round((recepte[produkts] * aboli - produkti[produkts]) * cenas[produkts], 2) 
+        print(f'* {recepte[produkts] * aboli - produkti[produkts]} {produkts} = {vajadziga_cena}€')
+        kopsumma += vajadziga_cena        
+
+print(f'\t- Kopsumma: {ceil(kopsumma)}€ -')
